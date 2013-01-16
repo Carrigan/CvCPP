412e79223b627d8202904fdc77a3a9daa0c7bfa9
Brian Carrigan <brian.c.carrigan@gmail.com>
2013-01-15 21:10:24 -0500

    Added license, readme.

--- C++/Source/main.cpp
+++ C++/Source/main.cpp
@@ -1,3 +1,26 @@
+/*
+  	Author: Brian Carrigan
+  	Date: 1/15/2013
+  	Email: brian.c.carrigan@gmail.com
+ 
+   This file is part of the C vs. CPP Project.
+
+    The C vs. CPP Project is free software: you 
+    can redistribute it and/or modify it under the terms of the GNU 
+    General Public License as published by the Free Software Foundation, 
+    either version 3 of the License, or (at your option) any later 
+    version.
+
+    The C vs. CPP Project is distributed in 
+    the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+    even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+    PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Evolutionary Blackjack Strategy Solver.  If not, 
+    see <http://www.gnu.org/licenses/>.
+ */
+
 #include "MorseCodeMessenger.h"
 #include "GPIO.h"
 #include "LED_rgb.h"
